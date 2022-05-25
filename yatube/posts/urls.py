78b73583decf811c54db5bf61f552615a6e324d4
Дмитрий Беликов <dmtrybelickov@gmail.com>
2022-05-25 19:09:18 +0300

    NameSpace

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 
 urlpatterns = [
     # Главная страница
+    path('posts/', views.group_list, name='group_list'),
     path('', views.index),
     path('group/<slug:slug>/', views.group_posts),
     path('group_list/', views.group_list)
