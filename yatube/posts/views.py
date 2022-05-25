4682aa5ac12d0c05521d94d38e39de0087f084fc
Дмитрий Беликов <dmtrybelickov@gmail.com>
2022-05-25 18:34:52 +0300

    словарь контекста

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,12 +4,27 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)                   
+    title = 'Социальная сеть для блогеров Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Это главная страница проекта Yatube',
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)                  
                         
         
 def group_list(request):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'text': 'Лев Толстой – зеркало русской революции'
+    }
+    return render(request, template, context)
+
 
 def group_posts(request, slug):
     return HttpResponse(f'Страницы сообществ {slug}')
